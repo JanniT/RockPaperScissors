3d1c2f21f572202a84e9a54daaf020ad57890e1b
JanniT <timoskainen.janni@gmail.com>
2022-12-02 23:33:52 +0200

    updated onClickRock

--- Windowed_Game/game.py
+++ Windowed_Game/game.py
@@ -28,12 +28,12 @@ def gameWindow():
 
     # Paper button
     photo_paper = PhotoImage(file="Pictures/paper.png")
-    paper_button = Button(root, text='Paper', image=photo_paper, command=onClickPaper)
+    paper_button = Button(root, text='Paper', image=photo_paper) #, command=onClickPaper)
     paper_button.grid(column=1, row=1)
 
     # Scissors button
     photo_scissors = PhotoImage(file="Pictures/scissors.png")
-    scissors_button = Button(root, text='Scissors', image=photo_scissors, command=onClickScissors)
+    scissors_button = Button(root, text='Scissors', image=photo_scissors) #, command=onClickScissors)
     scissors_button.grid(column=2, row=1)
 
 
@@ -42,11 +42,11 @@ def gameWindow():
 
 def onClickRock(): 
     root = Tk()
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(1, weight=2)
+    root.columnconfigure(0, weight=10)
+    root.rowconfigure(1, weight=10)
 
     # Creating the grid size
-    root.geometry("500x700")
+    root.geometry("400x250")
     root.title("Rock")
 
     # Back button
@@ -56,41 +56,32 @@ def onClickRock():
     choise_text = ttk.Label(root, text="You chose ROCK!", font=('Helvetica 20 bold'))
     choise_text.grid(column=0, row=0)
 
-    userChoise = "R"
+    userChoise = "Rock"
     computer_choise = computer_Choise() 
 
-    computer_text = ttk.Label(root, text=(f"Computer chose {computer_choise}!"), font=('Helvetica 20 bold'))
+    computer_text = ttk.Label(root, text=(f"Computer chose {computer_choise}!"), font=('Helvetica 15 bold'))
     computer_text.grid(column=0, row=1)
-
-
-    while(True): 
-
-        if (userChoise == (computer_choise)): 
-            choise_text = ttk.Label(root, text="It's a tie", font=('Helvetica 20 bold'))
-            computer_text.grid(column=0, row=2)
-            break
-        
-        elif (computer_choise == 'R' and userChoise == 'S'): 
-            choise_text = ttk.Label(root, text="Scissors beats rock, computer wins!", font=('Helvetica 20 bold'))
-            computer_text.grid(column=0, row=2)
-            break
-
-        elif (computer_choise == 'P' and userChoise == 'R'):
-            choise_text = ttk.Label(root, text="Scissors beats rock, computer wins!", font=('Helvetica 20 bold'))
-            computer_text.grid(column=0, row=2)
-            break
-
-        elif (computer_choise == 'S' and userChoise == 'P'):
-            choise_text = ttk.Label(root, text="Scissors beats rock, computer wins!", font=('Helvetica 20 bold'))
-            computer_text.grid(column=0, row=2)
-            break
-
-        else: 
-            choise_text = ttk.Label(root, text="You win!", font=('Helvetica 20 bold'))
-            computer_text.grid(column=0, row=2)
-            break
-
+ 
+    if (userChoise == computer_choise): 
+        tie_text = ttk.Label(root, text="It's a tie", font=('Helvetica 15 bold'))
+        tie_text.grid(column=0, row=2)
     
+    elif (computer_choise == "Rock" and userChoise == "Scissors"): 
+        computer_text1 = ttk.Label(root, text="Scissors beats rock,\ncomputer wins!", font=('Helvetica 15 bold'))
+        computer_text1.grid(column=0, row=2)
+
+    elif (computer_choise == "Paper" and userChoise == "Rock"):
+        computer_text2 = ttk.Label(root, text="Scissors beats rock,\ncomputer wins!", font=('Helvetica 15 bold'))
+        computer_text2.grid(column=0, row=2)
+        
+    elif (computer_choise == "Scissors" and userChoise == "Paper"):
+        computer_text3 = ttk.Label(root, text="Scissors beats rock,\ncomputer wins!", font=('Helvetica 15 bold'))
+        computer_text3.grid(column=0, row=2)
+
+    else: 
+        win_text = ttk.Label(root, text="You win!", font=('Helvetica 15 bold'))
+        win_text.grid(column=0, row=2)
+
     root.after(5000, lambda:root.destroy())
     return 
 
@@ -106,7 +97,7 @@ def computer_Choise():
     else:
         return "Scissors"
 
-
+    
 # def onClickPaper(): 
 #     root = Tk()
 #     root.columnconfigure(0, weight=1)
